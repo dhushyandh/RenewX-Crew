be33f16620231abd3d618711aae5b161a8eb6089
Your Name <dhushyandhneduncheziyan4896@gmail.com>
2026-09-13 20:35:31 +0530

    clerk webhoook updated

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,12 @@ import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import { clerkWebhook } from "./controllers/webhooks.js";
 
 const app = express();
 await connectDB()
+
+app.post('/api/clerk', express.raw({ type: 'application/json' }), clerkWebhook)
 
 
 // Middleware
@@ -13,6 +16,9 @@ app.use(cors())
 app.use(express.json());
 app.use(clerkMiddleware());
 
+app.get("/health", (req: Request, res: Response) => {
+    return res.send("OK");
+});
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
