370a60f7b14f8e9717c48b14766205b081a3412b
DHUSHYANDH <147026111+dhushyandh@users.noreply.github.com>
2026-09-18 15:47:47 +0530

    security: authenticate before product uploads

--- server/routes/productsRoutes.ts
+++ server/routes/productsRoutes.ts
@@ -1,21 +1,16 @@
-
 import express from 'express'
 import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from '../controllers/ProductController.js';
 import { authorize, protect } from '../middlewares/auth.js';
 import upload from '../middlewares/upload.js';
 
-
 const productRoutes = express.Router();
 
-productRoutes.get('/', getProducts)
-
+productRoutes.get('/', getProducts);
 productRoutes.get('/:id', getProduct);
 
-productRoutes.post('/', upload.array('images', 5), protect, authorize('admin'), createProduct)
-
-productRoutes.put('/:id', upload.array('images', 5), protect, authorize('admin'), updateProduct)
-
-productRoutes.delete('/:id', protect, authorize('admin'), deleteProduct)
-
+// Authenticate and authorize before accepting multipart data into memory.
+productRoutes.post('/', protect, authorize('admin'), upload.array('images', 5), createProduct);
+productRoutes.put('/:id', protect, authorize('admin'), upload.array('images', 5), updateProduct);
+productRoutes.delete('/:id', protect, authorize('admin'), deleteProduct);
 
 export default productRoutes;
