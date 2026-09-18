a70d87f82c44065ed9095e4400c0babada26173e
DHUSHYANDH <147026111+dhushyandh@users.noreply.github.com>
2026-09-18 12:36:09 +0530

    Harden cart, address, order and notification routes

--- server/routes/notificationRoutes.ts
+++ server/routes/notificationRoutes.ts
@@ -1,7 +1,5 @@
-import express, { Request, Response, NextFunction } from "express";
-import { protect } from "../middlewares/auth.js";
-import { getAuth } from "@clerk/express";
-import User from "../models/user.js";
+import express from "express";
+import { authorize, protect } from "../middlewares/auth.js";
 import {
     registerPushToken,
     unregisterPushToken,
@@ -12,24 +10,10 @@ import {
 
 const notificationRoutes = express.Router();
 
-// Optional auth middleware for production test endpoint
-const optionalProtect = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const auth = getAuth(req);
-        const userId = auth?.userId || (req as any).auth?.userId;
-        if (userId) {
-            const user = await User.findOne({ clerkId: userId });
-            if (user) (req as any).user = user;
-        }
-    } catch (_) {}
-    next();
-};
-
-// Endpoints
 notificationRoutes.post("/register", protect, registerPushToken);
 notificationRoutes.delete("/unregister", protect, unregisterPushToken);
 notificationRoutes.get("/preferences", protect, getPreferences);
 notificationRoutes.put("/preferences", protect, updatePreferences);
-notificationRoutes.post("/test", optionalProtect, sendTestNotification);
+notificationRoutes.post("/test", protect, authorize("admin"), sendTestNotification);
 
 export default notificationRoutes;
