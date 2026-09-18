bd4cc6c405c00cb4d40ebe8d147ac494f8a409b4
DHUSHYANDH <147026111+dhushyandh@users.noreply.github.com>
2026-09-18 12:35:52 +0530

    Harden cart, address, order and notification routes

--- server/routes/orderRoutes.ts
+++ server/routes/orderRoutes.ts
@@ -1,18 +1,13 @@
 import express from 'express'
 import { authorize, protect } from '../middlewares/auth.js'
-import { getOrder, getOrders,createOrder,updateOrderStatus, getAllOrders } from '../controllers/ordersController.js'
-
+import { getOrder, getOrders, createOrder, updateOrderStatus, getAllOrders } from '../controllers/ordersController.js'
 
 const orderRoutes = express.Router()
 
 orderRoutes.get('/', protect, getOrders)
-
-orderRoutes.get('/:id', protect, getOrder)
-
-orderRoutes.post('/', protect, createOrder)
-
-orderRoutes.put('/:id/status', protect, authorize('admin'), updateOrderStatus)
-
 orderRoutes.get('/admin/all', protect, authorize('admin'), getAllOrders)
+orderRoutes.get('/:id', protect, getOrder)
+orderRoutes.post('/', protect, createOrder)
+orderRoutes.put('/:id/status', protect, authorize('admin'), updateOrderStatus)
 
 export default orderRoutes
